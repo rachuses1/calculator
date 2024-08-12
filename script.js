07cfc448aed398ff772175a06765a27fe30c1c49
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-12 11:51:28 +1000

    set up variables for numbers and operations in the javascript file

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-const display = document.querySelector("div.display")
+const screen = document.querySelector('.screen');
+const numbers = document.querySelectorAll(".buttons");
 
-const numbers = document.querySelectorAll(".buttons")
+const operator = document.querySelectorAll('.operators');
+const add = document.querySelector('.add');
+const multiply = document.querySelector('.multiply');
+const subtract = document.querySelector('.subtract');
+const divide = document.querySelector('.divide');
 
-numbers.addEventListener("click",display);
+
+numbers.forEach((number) =>{number.addEventListener("click",display)});
 
 function display() {
-    
+    screen.textContent = numbers[0].innerHTML;
+}
+
+let a = 10;
+let b = 2;
+let operation;
+
+add.addEventListener("click",() => operation = "+");
+multiply.addEventListener("click", () => operation = "*");
+divide.addEventListener("click", () => operation = "/");
+subtract.addEventListener("click",() => operation = "-");
+
+function operate(a, b, operation) {
+    if (operation === "+") {
+        addition(a, b);
+    } else if (operation === "*") {
+        multiplication(a, b);
+    } else if (operation === "/") {
+        division(a, b);
+    } else if (operation === "-") {
+        subtraction(a, b);
+    }
+}
+
+function addition(a, b) {
+    return a + b;
+}
+
+function multiplication (a, b) {
+    return a * b;
+} 
+
+function subtraction (a, b) {
+    return a - b;
+}
+
+function division (a, b) {
+    return a / b;
 }
