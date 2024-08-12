4b01be7131744989daad74d0e0aa48bb79a7d977
rachuses <133572545+rachuses1@users.noreply.github.com>
2024-08-12 10:11:53 +1000

    set up the basic layout and buttons

---
+++ script.js
@@ -0,0 +1,9 @@
+const display = document.querySelector("div.display")
+
+const numbers = document.querySelectorAll(".buttons")
+
+numbers.addEventListener("click",display);
+
+function display() {
+    
+}
